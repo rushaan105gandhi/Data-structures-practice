c42f3a42bbf92e5827172775de7000f17983df84
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-04 22:17:31 +0530

    duplicates in array code studio

---
+++ Arrays/duplicates_in_array_codestd.cpp
@@ -0,0 +1,11 @@
+int findDuplicate(vector<int> &arr) 
+{
+    int ans = 0;
+    for (int i = 0; i < arr.size(); i++){
+        ans = ans^arr[i];
+    }
+	for (int i = 0; i < arr.size(); i++){
+        ans = ans^i;
+    }
+    return ans;
+}
