f91c43a2f0cfbf134f1bc23005f26af80458c84e
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-19 15:57:04 +0530

    book allocation problem

--- Arrays/pages_allocation_problem.cpp
+++ Arrays/pages_allocation_problem.cpp
@@ -1,41 +1,73 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-bool isPossible(vector<int> time, int m, int n, int mid){
+bool isPossible(vector<int>&arr, int m, int n, long long mid){
+
 	int chapter = 1;
-	int sum = 0;
-	for(int i = 0; i<n;i++){
-		if(sum + time[i] <=mid){
-			sum = sum+time[i];
+
+	long long sum = 0;
+
+	for(int i = 0; i<m;i++){
+
+		if(sum + arr[i] <=mid){
+
+			sum = sum+arr[i];
+
 		}
+
 		else{
+
 			chapter++;
-			if(chapter > m || time[i]>mid){
+
+			if(chapter > n || arr[i]>mid){
+
 				return false;
+
 			}
-			sum = time[i];
+
+			sum = 0;
+
+			sum = arr[i];
+
 		}
+
 	}
+
 	return true;
+
 }
+
+
 long long ayushGivesNinjatest(int n, int m, vector<int> time) 
 {	
-	int s = 0;
-	int sum = 0;
 
-	for (int i = 0; i<n;i++){
+	long long s = 0;
+
+	long long sum = 0;
+
+	for (int i = 0; i<m;i++){
+
 		sum = sum+time[i];
+
 	}
 
-	int e = sum;
-	int ans = 0;
-	int mid = s + (e-s)/2;
+
+	long long e = sum;
+
+	long long ans = 0;
+
+	long long mid = s + (e-s)/2;
 
 	while(s<=e){
+
 		if(isPossible(time, m,n, mid)){
+
 			ans = mid;
+
 			e=mid-1;
+
 		}
+
 		else{
 			s = mid+1;
 		}
