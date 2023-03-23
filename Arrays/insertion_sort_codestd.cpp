ea904d0af873f616f0c0c78d7dddb665cd618814
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-24 03:04:59 +0530

    insertion sort

---
+++ Arrays/insertion_sort_codestd.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int InsertionSort(vector<int> &arr, int n){
+    for(int i = 0; i<n;i++){
+
+        int j = i - 1;
+        
+        int temp = arr[i];
+
+        for(; j >= 0; j--){
+
+            if(arr[j]> temp){
+                arr[j+1] + arr[j];
+            }
+            else{
+                break;
+            }
+        }
+        arr[j+1] = temp;
+    }
+}
