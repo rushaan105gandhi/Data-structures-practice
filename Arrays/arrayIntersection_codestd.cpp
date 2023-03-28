22612f513cb361a53c34d534b9e55ed4c659d4f9
Rushaan Gandhi <rushaansps@gmail.com>
2023-03-28 23:12:50 +0530

    ..

--- Arrays/arrayIntersection_codestd.cpp
+++ Arrays/arrayIntersection_codestd.cpp
@@ -24,3 +24,31 @@ vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, i
 	}
 	return ans;
 }
+/*
+#include <iostream> 
+#include <vector>
+
+
+using namespace std;
+
+vector<int> findArrayIntersection(vector<int> &arr1, int n, vector<int> &arr2, int m)
+{
+	int i = 0,j = 0;
+	//aray intersection
+	vector <int> ans;
+	while(i<n && j<m){
+		if (arr1[i] == arr2[j]){
+			ans.push_back(arr1[i]);
+			i++;
+			j++;
+		}
+		else if(arr1[i]<arr2[j]){
+			i++;
+		}
+		else{
+			j++;
+		}
+	}
+	return ans;
+}
+*/
