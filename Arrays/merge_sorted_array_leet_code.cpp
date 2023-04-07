00d3623b6126b6351f35bdc4312a2d397a239edc
Rushaan Gandhi <rushaansps@gmail.com>
2023-04-07 22:11:37 +0530

    leet code: merge sorted arrays

--- Arrays/merge_sorted_array_leet_code.cpp
+++ Arrays/merge_sorted_array_leet_code.cpp
@@ -2,24 +2,29 @@
 using namespace std;
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
         int i = m-1;
         int res = n+m-1;
         int j = n-1;
 
-        while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
+        while(i>=0 && j>=0)
+        {
+            if(nums1[i]>nums2[j])
+            {
                 nums1[res] = nums1[i];
                 res--;
                 i--;
             }
-            else{
+            else
+            {
                 nums1[res] = nums2[j];
                 res--;
                 j--;
             }
         }
-        while(j>=0){
+        while(j>=0)
+        {
             nums1[res] = nums2[j];
             res--;
             j--;
