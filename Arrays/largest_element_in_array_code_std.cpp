a4d07ea231dc3ded06ad831e9c930728cf2996ae
Rushaan Gandhi <rushaansps@gmail.com>
2023-08-19 16:08:09 +0530

    largest element in array

---
+++ Arrays/largest_element_in_array_code_std.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h> 
+using namespace std;
+int largestElement(vector<int> &arr, int n) {
+    int largest = arr[0];
+    for(int i = 0; i< n; i++){
+        if(arr[i] > largest){
+            largest = arr[i];
+        }
+    }
+    return largest;
+}
