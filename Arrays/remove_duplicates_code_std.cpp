784413b7f568735841635fd73c01becb97f4c5e4
rushaan gandhi <rushaansps@gmail.com>
2023-08-26 17:23:04 +0530

    remove duplicates from sorted array code studio

---
+++ Arrays/remove_duplicates_code_std.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int> &arr, int n) {
+	int i = 0;
+	
+	for(int j = 1; j<n; j++){
+		if(arr[i] != arr[j]){
+			arr[i+1] = arr[j];
+			i++;
+		}
+	}
+	return i+1;
+}
