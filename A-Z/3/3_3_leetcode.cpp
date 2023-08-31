5cd68637b49e001242be3cbfc3f329715890ddba
rushaan gandhi <rushaansps@gmail.com>
2023-08-31 13:53:37 +0530

    3.1.3

---
+++ A-Z/3/3_3_leetcode.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        int n=nums.size();
+        int count=0;
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                count++;
+            }
+        }
+        if(nums[n-1]>nums[0]){
+            count++;
+        }
+        return count<=1;
+    }
+};
