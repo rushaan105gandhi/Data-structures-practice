64dff080caf42bd2f782057fc4bcf4ff638a88e9
rushaan gandhi <rushaansps@gmail.com>
2023-08-31 13:48:09 +0530

    3.1.1

--- A-Z/3/3_1_codestd.cpp
+++ A-Z/3/3_1_codestd.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h> 
 using namespace std;
-
+//https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 int largestElement(vector<int> &arr, int n) {
     int largest = arr[0];
     for(int i = 0; i< n; i++){
