d550117e63a66a81d3f4474fd45fd8e54a592991
rushaan gandhi <rushaansps@gmail.com>
2023-09-19 01:02:50 +0530

    union of 2 sorted arrays optimal and brute force approach

--- A-Z/3/3_9_codestd.cpp
+++ A-Z/3/3_9_codestd.cpp
@@ -6,6 +6,38 @@ using namespace std;
     https://www.codingninjas.com/studio/problems/sorted-array_6613259?utm_source=striver&utm_medium=website&utm_campaign=codestudio_a_zcourse
 */
 
+/*
+    
+    Brute Force Approach: Using Ordered Map
+    Complexity Analysis:
+        Time Complexity: O(n*log(n)) + O(m*log(m))
+        Space Complexity: O(m+n) + O(m+n)
+
+    vector < int > sortedArray(vector < int > a, vector < int > b) 
+    {
+        map <int,int> mpp;
+        vector<int> Union;
+        for(int i = 0; i<a.size();i++){
+            mpp[a[i]]++;
+        }
+        for(int i = 0; i<b.size();i++){
+            mpp[b[i]]++;
+        }
+
+        for(auto &it: mpp){
+            Union.push_back(it.first);
+        }
+        return Union;
+    }
+*/
+
+/*
+    Optimal Approach Using 2 pointer technique
+    Complexity Analysis:
+        Time Complexity: O(n+m)    
+        Space Complexity: O(n+m) : To return the answer, not for Solving
+*/
+
 vector < int > sortedArray(vector < int > a, vector < int > b) {
     int n1 = a.size();
     int n2 = b.size();
