1e98ca57b259b780fdb462200b3b78c158f48a26
rushaan gandhi <rushaansps@gmail.com>
2023-09-26 23:15:42 +0530

    Find Largest Element in an array

--- A-Z/3/3_1_codestd.cpp
+++ A-Z/3/3_1_codestd.cpp
@@ -1,6 +1,15 @@
 #include <bits/stdc++.h> 
 using namespace std;
-//https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+/*
+    Find the largest element in an array
+    
+    Problem Link:
+    https://www.codingninjas.com/studio/problems/largest-element-in-the-array-largest-element-in-the-array_5026279?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+
+    Complexity Analysis:
+        Time Complexity: O(N)
+        Space Complexity: O(1)
+*/
 int largestElement(vector<int> &arr, int n) {
     int largest = arr[0];
     for(int i = 0; i< n; i++){
