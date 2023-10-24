221fe2b055f27a3e872194c713f6d0da5d4873a2
rushaan gandhi <rushaansps@gmail.com>
2023-10-24 18:30:03 +0530

    count subarrays w given sum codestd

---
+++ A-Z/3/med_13_codestd.cpp
@@ -0,0 +1,23 @@
+
+/*
+    Count subarrays with given sum
+    https://www.codingninjas.com/studio/problems/subarray-sums-i_1467103?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=1
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int findAllSubarraysWithGivenSum(vector < int > & arr, int k) {
+    map<int,int> mpp;
+    mpp[0] = 1;
+
+    int presum = 0, cnt = 0;
+
+    for(int i = 0; i<arr.size(); i++)
+    {
+        presum += arr[i];
+        int remove = presum - k;
+        cnt+=mpp[remove];
+        mpp[presum] += 1;
+    }
+    return cnt;
+}
