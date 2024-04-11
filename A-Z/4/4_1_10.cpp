4dae0a2dd2b915f94274997bafda44e06334bc4e
rushaan gandhi <rushaansps@gmail.com>
2024-04-11 11:32:32 +0530

    find minimum in rotaed array: optimized

---
+++ A-Z/4/4_1_10.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>& arr)
+{
+	int low = 0;
+	int high = arr.size()-1;
+	int ans = INT_MAX;
+
+	while (low <= high) {
+		int mid = (high + low)/2;
+
+		if (arr[low] <= arr[high]) {
+			ans = min(ans, arr[low]);
+		}
+		
+		if (arr[low] <= arr[mid]){
+			ans = min(ans, arr[low]);
+			low = mid + 1;
+		}
+		else if(arr[mid] <= arr[high]) {
+			ans = min(arr[mid], ans);
+			high = mid - 1;
+		}
+	}
+	return ans;
+}
