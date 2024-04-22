6d7de57c2a99eaf449166c157e5444430be76f46
rushaan gandhi <rushaansps@gmail.com>
2024-04-22 15:27:16 +0530

    Middle of the linked List: Optimal Approach (Fast and slow pointer/ TortoiseHair Method)

---
+++ A-Z/5/5_1_leetcode.cpp
@@ -0,0 +1,27 @@
+/*
+    Middle of the linked List: Optimal Approach (Fast and slow pointer/ TortoiseHair Method)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast != NULL && fast -> next != NULL) {
+            slow = slow -> next;
+            fast = fast -> next -> next;
+        }
+        return slow;
+    }
+};
